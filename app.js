52a9969319ffd4d4b9ac17cc4c1550321dde11fc
Vanzus <vanzus@gmail.com>
2023-11-11 22:49:48 +0000

    corrigindo o app js

--- app.js
+++ app.js
@@ -2,3 +2,4 @@ console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("Testando a conexão com o banco de dados")
 // mais um console log teste:
 console.log("fiz esse comid para teste")
+console.log("Enviando para o Github")
